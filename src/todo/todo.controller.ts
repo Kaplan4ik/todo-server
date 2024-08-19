0c5557bcd6f61ac500dd23738a7dea2018fa36ae
Vitalii Kostiukov <vitalii.kostiukov@mev.com>
2024-08-19 20:00:57 +0300

    Add guard for another endpoints

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -17,12 +17,14 @@ import { TodoResolver } from './todo.resolver';
 export class TodoController {
   constructor(private readonly todoResolver: TodoResolver) {}
 
+  //TODO: Implement middleware to check if user is authorized to access
   @UseGuards(AuthorizationGuard)
   @Get()
   async getTodos(): Promise<TodoEntity[]> {
     return await this.todoResolver.getTodos();
   }
 
+  @UseGuards(AuthorizationGuard)
   @Post()
   async createTodo(
     @Body()
@@ -31,11 +33,13 @@ export class TodoController {
     return await this.todoResolver.createTodo(title);
   }
 
+  @UseGuards(AuthorizationGuard)
   @Delete('/:id')
   deleteTodo(@Param('id') id: string): Promise<TodoEntity[]> {
     return this.todoResolver.deleteTodo(Number(id));
   }
 
+  @UseGuards(AuthorizationGuard)
   @Patch('/:id')
   updateTodo(@Param('id') id: string): Promise<TodoEntity[]> {
     return this.todoResolver.updateTodo(Number(id));
