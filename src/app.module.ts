1a138a4d8f4dbe7150c89f7ed3984e40e28dfbc9
Vitalii Kostiukov <vitalii.kostiukov@mev.com>
2024-10-04 09:30:16 +0300

    Prepare to deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AuthorizationModule } from './authorization/authorization.module';
 import { TodoModule } from './todo/todo.module';
 import { UserModule } from './user/user.module';
@@ -8,23 +7,25 @@ import { UserModule } from './user/user.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_DATABASE'),
-        port: configService.get<number>('DB_PORT'),
-        entities: [__dirname + '/**/*.entity{.ts}'],
-        // TODO: Remove after deployment
-        synchronize: true,
-        autoLoadEntities: true,
-        logging: true,
-      }),
-    }),
+    //TODO: Uncomment after deployment
+
+    // TypeOrmModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   inject: [ConfigService],
+    //   useFactory: async (configService: ConfigService) => ({
+    //     type: 'postgres',
+    //     host: configService.get<string>('DB_HOST'),
+    //     username: configService.get<string>('DB_USERNAME'),
+    //     password: configService.get<string>('DB_PASSWORD'),
+    //     database: configService.get<string>('DB_DATABASE'),
+    //     port: configService.get<number>('DB_PORT'),
+    //     entities: [__dirname + '/**/*.entity{.ts}'],
+    //     // TODO: Remove after deployment
+    //     synchronize: true,
+    //     autoLoadEntities: true,
+    //     logging: true,
+    //   }),
+    // }),
     TodoModule,
     AuthorizationModule,
     UserModule,
