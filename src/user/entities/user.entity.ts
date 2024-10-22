bc2cdb4e7104048e8f82aaf6c43c9c4632becc2a
Kaplan4ik <48730789+Kaplan4ik@users.noreply.github.com>
2024-10-22 18:46:05 +0300

    Revert "Create work with DB (#15)" (#16)
    This reverts commit 3a79126b3597638a29f90b530458ca9a6bb71746.
    
    Co-authored-by: Vitalii Kostiukov <vitalii.kostiukov@mev.com>

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,14 +1,14 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
-import { TodoEntity } from '../../todo/entities/todo.entity';
-
-@Entity('users')
-export class UserEntity {
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  @Column()
-  auth_user_id: string;
-
-  @OneToMany(() => TodoEntity, (todo) => todo.user)
-  todos: TodoEntity[];
-}
+// import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+// import { TodoEntity } from '../../todo/entities/todo.entity';
+//
+// @Entity('users')
+// export class UserEntity {
+//   @PrimaryGeneratedColumn()
+//   id: number;
+//
+//   @Column()
+//   auth_user_id: string;
+//
+//   @OneToMany(() => TodoEntity, (todo) => todo.user)
+//   todos: TodoEntity[];
+// }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,28 +1,29 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { UserEntity } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  constructor(
-    @InjectRepository(UserEntity)
-    private readonly userRepository: Repository<UserEntity>,
-  ) {}
+  // constructor(
+  //   @InjectRepository(UserEntity)
+  //   private readonly userRepository: Repository<UserEntity>,
+  // ) {}
 
-  async currentUser(userId: string): Promise<UserEntity> {
-    const user = await this.getUserById(userId);
-    if (user) {
-      return user;
-    }
-
-    await this.userRepository.save({ auth_user_id: userId });
-    return await this.getUserById(userId);
+  async currentUser(userId: string): Promise<string> {
+    console.group('user.service.ts', 'currentUser', '14');
+    console.log('DEBUG1', userId);
+    console.groupEnd();
+    return userId;
+    // const user = await this.getUserById(userId);
+    // if (user) {
+    //   return user;
+    // }
+    //
+    // await this.userRepository.save({ auth_user_id: userId });
+    // return await this.getUserById(userId);
   }
 
-  private async getUserById(userId: string): Promise<UserEntity> {
-    return await this.userRepository.findOne({
-      where: { auth_user_id: userId },
-    });
-  }
+  // private async getUserById(userId: string): Promise<string> {
+  //   return await this.userRepository.findOne({
+  //     where: { auth_user_id: userId },
+  //   });
+  // }
 }
