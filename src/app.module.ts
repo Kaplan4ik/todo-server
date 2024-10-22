3a79126b3597638a29f90b530458ca9a6bb71746
Kaplan4ik <48730789+Kaplan4ik@users.noreply.github.com>
2024-10-22 17:48:40 +0300

    Create work with DB (#15)
    Co-authored-by: Vitalii Kostiukov <vitalii.kostiukov@mev.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthorizationModule } from './authorization/authorization.module';
 import { TodoModule } from './todo/todo.module';
 import { UserModule } from './user/user.module';
@@ -8,25 +9,24 @@ import { HealthCheckModule } from './health-check/health-check.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    //TODO: Uncomment after deployment!
 
-    // TypeOrmModule.forRootAsync({
-    //   imports: [ConfigModule],
-    //   inject: [ConfigService],
-    //   useFactory: async (configService: ConfigService) => ({
-    //     type: 'postgres',
-    //     host: configService.get<string>('DB_HOST'),
-    //     username: configService.get<string>('DB_USERNAME'),
-    //     password: configService.get<string>('DB_PASSWORD'),
-    //     database: configService.get<string>('DB_DATABASE'),
-    //     port: configService.get<number>('DB_PORT'),
-    //     entities: [__dirname + '/**/*.entity{.ts}'],
-    //     // TODO: Remove after deployment
-    //     synchronize: true,
-    //     autoLoadEntities: true,
-    //     logging: true,
-    //   }),
-    // }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        port: configService.get<number>('DB_PORT'),
+        entities: [__dirname + '/**/*.entity{.ts}'],
+        // TODO: Remove after implementing migrations
+        synchronize: true,
+        autoLoadEntities: true,
+        logging: true,
+      }),
+    }),
     TodoModule,
     AuthorizationModule,
     UserModule,
