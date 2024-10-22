cca04334a89e64067dd28e5c196036b37a42686e
Vitalii Kostiukov <vitalii.kostiukov@mev.com>
2024-10-22 17:47:06 +0300

    Create work with DB

--- src/todo/entities/todo.entity.ts
+++ src/todo/entities/todo.entity.ts
@@ -1,17 +1,17 @@
-// import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
-// import { UserEntity } from '../../user/entities/user.entity';
-//
-// @Entity('todos')
-// export class TodoEntity {
-//   @PrimaryGeneratedColumn()
-//   id: number;
-//
-//   @Column()
-//   title: string;
-//
-//   @Column({ default: false })
-//   completed: boolean;
-//
-//   @ManyToOne(() => UserEntity, (user) => user.todos)
-//   user: UserEntity;
-// }
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { UserEntity } from '../../user/entities/user.entity';
+
+@Entity('todos')
+export class TodoEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column({ default: false })
+  completed: boolean;
+
+  @ManyToOne(() => UserEntity, (user) => user.todos)
+  user: UserEntity;
+}
--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -11,6 +11,7 @@ import {
 } from '@nestjs/common';
 import { AuthorizationGuard } from '../authorization/authorization.guard';
 import { CreateTodoDto } from './dto/create-todo.dto';
+import { TodoEntity } from './entities/todo.entity';
 import { TodoService } from './todo.service';
 
 @UseGuards(AuthorizationGuard)
@@ -20,7 +21,7 @@ export class TodoController {
 
   //TODO: Create interface for request
   @Get()
-  async getTodos(@Req() request: any): Promise<string[]> {
+  async getTodos(@Req() request: any): Promise<TodoEntity[]> {
     const userId = request.userId;
     return await this.todoService.getTodos(userId);
   }
@@ -29,19 +30,25 @@ export class TodoController {
   async createTodo(
     @Body() title: CreateTodoDto,
     @Req() request: any,
-  ): Promise<string[]> {
+  ): Promise<TodoEntity[]> {
     const userId = request.userId;
     return await this.todoService.createTodo(title, userId);
   }
 
   @Delete('/:id')
-  deleteTodo(@Req() request: any, @Param('id') id: string): Promise<string[]> {
+  deleteTodo(
+    @Req() request: any,
+    @Param('id') id: string,
+  ): Promise<TodoEntity[]> {
     const userId = request.userId;
     return this.todoService.deleteTodo(userId, Number(id));
   }
 
   @Patch('/:id')
-  updateTodo(@Req() request: any, @Param('id') id: string): Promise<string[]> {
+  updateTodo(
+    @Req() request: any,
+    @Param('id') id: string,
+  ): Promise<TodoEntity[]> {
     const userId = request.userId;
     return this.todoService.updateTodo(userId, Number(id));
   }
