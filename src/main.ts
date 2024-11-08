5b016b3737b4bea8941219ad8db62fa12618c3a4
Vitalii Kostiukov <vitalii.kostiukov@mev.com>
2024-11-08 13:34:54 +0200

    Prepare for deploy

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
-import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  const config = app.get(ConfigService);
-  const port = config.get('API_PORT');
-  await app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  await app.listen(8080, () => {
+    console.log(`Server is running on http://localhost:8080`);
   });
 }
 
