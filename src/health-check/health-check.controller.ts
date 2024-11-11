49b2f022055329bf448199f7d033089f19941462
Vitalii Kostiukov <vitalii.kostiukov@mev.com>
2024-11-11 13:42:17 +0200

    Cleanup

--- src/health-check/health-check.controller.ts
+++ src/health-check/health-check.controller.ts
@@ -1,12 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 
 @Controller('health-check')
 export class HealthCheckController {
-  constructor(private configService: ConfigService) {}
   @Get()
   async healthCheck(): Promise<string> {
-    const test = this.configService.get<string>('AUTH0_DOMAIN');
-    return `Health check is OK!!!! - ${test}`;
+    return `Health check is OK!`;
   }
 }
