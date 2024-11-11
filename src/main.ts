1a5a74e30c82f2f2494ca6e05551f4db8cbf38d9
Vitalii Kostiukov <vitalii.kostiukov@mev.com>
2024-11-11 09:27:57 +0200

    Revert "Prepare for deploy"
    This reverts commit 5b016b3737b4bea8941219ad8db62fa12618c3a4.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(8080, () => {
-    console.log(`Server is running on http://localhost:8080`);
+  const config = app.get(ConfigService);
+  const port = config.get('API_PORT');
+  await app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
   });
 }
 
---
+++ src/todo/todo.controller.ts
@@ -0,0 +1,55 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthorizationGuard } from '../authorization/authorization.guard';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { TodoEntity } from './entities/todo.entity';
+import { TodoService } from './todo.service';
+
+@UseGuards(AuthorizationGuard)
+@Controller('api/todo')
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+
+  //TODO: Create interface for request
+  @Get()
+  async getTodos(@Req() request: any): Promise<TodoEntity[]> {
+    const userId = request.userId;
+    return await this.todoService.getTodos(userId);
+  }
+
+  @Post()
+  async createTodo(
+    @Body() title: CreateTodoDto,
+    @Req() request: any,
+  ): Promise<TodoEntity[]> {
+    const userId = request.userId;
+    return await this.todoService.createTodo(title, userId);
+  }
+
+  @Delete('/:id')
+  deleteTodo(
+    @Req() request: any,
+    @Param('id') id: string,
+  ): Promise<TodoEntity[]> {
+    const userId = request.userId;
+    return this.todoService.deleteTodo(userId, Number(id));
+  }
+
+  @Patch('/:id')
+  updateTodo(
+    @Req() request: any,
+    @Param('id') id: string,
+  ): Promise<TodoEntity[]> {
+    const userId = request.userId;
+    return this.todoService.updateTodo(userId, Number(id));
+  }
+}
