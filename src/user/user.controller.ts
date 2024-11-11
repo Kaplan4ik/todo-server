f1f30cd03b6554ba8e0eecf10ef36aae151eee2f
Vitalii Kostiukov <vitalii.kostiukov@mev.com>
2024-11-11 13:23:45 +0200

    Check endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,29 +1,18 @@
-import {
-  Controller,
-  Get,
-  InternalServerErrorException,
-  Req,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthorizationGuard } from '../authorization/authorization.guard';
-import { UserEntity } from './entities/user.entity';
-import { UserService } from './user.service';
 
 @UseGuards(AuthorizationGuard)
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor() {}
 
   //TODO: Create interface for request
   @Get()
-  async currentUser(@Req() request: any): Promise<UserEntity> {
-    try {
-      const userId = request.userId;
+  async currentUser(@Req() request: any): Promise<string> {
+    const userId = request.userId;
 
-      return await this.userService.currentUser(userId);
-    } catch (e) {
-      console.error('Error fetching user:', e);
-      throw new InternalServerErrorException('Could not fetch user');
-    }
+    return userId;
+
+    // return await this.userService.currentUser(userId);
   }
 }
